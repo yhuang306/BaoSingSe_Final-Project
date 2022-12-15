40790087fb6166b4c4f413981dd8405f227d0802
avrubio <ariadna.vanegas@gmail.com>
2022-12-15 13:58:19 -0500

    Task 10: Step 1: Using fetch to POST the new Item

--- js/productsController.js
+++ js/productsController.js
@@ -42,4 +42,23 @@ class ProductsController {
       }
     }
   }
+
+  save(name, description, price, imageUrl) {
+    const data = { name, description, price, imageUrl };
+
+    fetch("http://localhost:8080/item", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 }
